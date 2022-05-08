d7dc3832bc6922ef4192773f80d82eb4dd2c79fd
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2022-05-08 19:33:21 +0100

    Users and readings apps and their models

---
+++ readings/models.py
@@ -0,0 +1,13 @@
+from curses.ascii import US
+from django.db import models
+from users.models import User
+
+class Reading(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    # Values
+    temperature = models.FloatField()
+    heart_rate = models.FloatField()
+    respiratory_rate = models.FloatField()
+    oxygen_saturation = models.FloatField()
+    blood_pressure = models.FloatField()
---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+
+class User(AbstractUser):
+    citizenId = models.IntegerField(unique=True, validators=[
+        MaxLengthValidator(8),
+        MinLengthValidator(8)
+    ])
+    # TODO: Remove other fields and make citizenId Primary Key
